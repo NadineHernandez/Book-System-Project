3ea4ab46be134f2a27c65e1ec8d9bb3660e87433
ace <allisterg@gmail.com>
2019-10-02 15:45:33 -0400

    fix pom file

--- note-queue-consumer/src/main/java/com/company/notequeueconsumer/feign/NoteFeignClient.java
+++ note-queue-consumer/src/main/java/com/company/notequeueconsumer/feign/NoteFeignClient.java
@@ -1,7 +1,7 @@
 package com.company.notequeueconsumer.feign;
 
 import com.company.notequeueconsumer.util.message.Note;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
