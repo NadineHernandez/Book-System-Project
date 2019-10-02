4371e5f3f55f45abba0a85cbc324f10308ccdb02
Nadine Hernandez <nrhernandez93@gmail.com>
2019-10-02 13:44:54 -0400

    dao tests completed

--- book-service/src/main/java/com/company/bookservice/DAO/BookImplemention.java
+++ book-service/src/main/java/com/company/bookservice/DAO/BookImplemention.java
@@ -1,9 +1,11 @@
 package com.company.bookservice.DAO;
 
 import com.company.bookservice.DTO.Book;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class BookImplemention implements BookDao {
     @Override
     public Book addBook(Book book) {
