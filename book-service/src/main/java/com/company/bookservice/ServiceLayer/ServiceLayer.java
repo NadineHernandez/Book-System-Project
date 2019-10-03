60d34fd1dbf19bad634c64cec36ddd81f249c48e
Nadine Hernandez <nrhernandez93@gmail.com>
2019-10-03 09:17:32 -0400

    added basic service layer without implementations

--- book-service/src/main/java/com/company/bookservice/ServiceLayer/ServiceLayer.java
+++ book-service/src/main/java/com/company/bookservice/ServiceLayer/ServiceLayer.java
@@ -1,5 +1,49 @@
 package com.company.bookservice.ServiceLayer;
 
+import com.company.bookservice.DAO.BookDao;
+import com.company.bookservice.DTO.Book;
+import com.company.bookservice.ViewModel.ViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
 public class ServiceLayer {
+
+    private BookDao dao;
+
+    @Autowired
+    public ServiceLayer(BookDao dao){
+        this.dao = dao;
+    }
+
+    @Transactional
+    public ViewModel saveBook(ViewModel viewModel){
+        return null;
+    }
+
+    private ViewModel buildViewModel(Book book){
+        return null;
+    }
+
+    public ViewModel findBook(int id){
+        return null;
+    }
+
+    public List<ViewModel> findAllBooks(){
+        return null;
+    }
+
+    @Transactional
+    public void updateBook(ViewModel viewModel){
+
+    }
+
+    @Transactional
+    public void removeBook(int id){
+
+    }
 
 }
