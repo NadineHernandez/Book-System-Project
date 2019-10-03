fe63fa34019229085433e1ad82cfd5fda925fe66
rfenix3 <richard.fenix@gmail.com>
2019-10-03 11:24:57 -0400

    Saved service layer test codes.

--- book-service/src/test/java/com/company/bookservice/service/ServiceLayerTest.java
+++ book-service/src/test/java/com/company/bookservice/service/ServiceLayerTest.java
@@ -4,6 +4,8 @@ import com.company.bookservice.DAO.BookDao;
 import com.company.bookservice.DAO.BookImplemention;
 import com.company.bookservice.DTO.Book;
 import com.company.bookservice.ViewModel.ViewModel;
+import com.company.bookservice.util.feign.NoteClient;
+import com.company.bookservice.util.messages.Note;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,37 +19,57 @@ public class ServiceLayerTest {
 
     private ServiceLayer serviceLayer;
     private BookDao bookDao;
+    private NoteClient client;
+
+    private void setUpNoteClientMock(){
+        client = mock(NoteClient.class);
+
+        Note note = new Note();
+        note.setNoteId(1);
+        note.setBookId(1);
+        note.setNote("Sample Note 1");
+
+        Note note2 = new Note();
+        note2.setBookId(1);
+        note2.setNote("Sample Note 1");
+
+        List<Note> nList = new ArrayList<>();
+        nList.add(note);
+
+        doReturn(nList).when(client).getNotesByBookfromDB(1);
+    }
 
     private void setUpBookDaoMock(){
         bookDao = mock(BookImplemention.class);
 
-        Book book = new Book();
-        book.setBookId(1);
-        book.setTitle("Sample Title");
-        book.setAuthor("Sample Author");
+        ViewModel viewModel = new ViewModel();
+        viewModel.setBookId(1);
+        viewModel.setTitle("Sample Title");
+        viewModel.setAuthor("Sample Author");
+        viewModel.setNotes(client.getNotesByBookfromDB(viewModel.getBookId()));
+
 
         Book book2 = new Book();
         book2.setTitle("Sample Title");
         book2.setAuthor("Sample Author");
 
-        List<Book> bList = new ArrayList<>();
-        bList.add(book);
+        List<ViewModel> bList = new ArrayList<>();
+        bList.add(viewModel);
 
-        doReturn(book).when(bookDao).addBook(book2);
-        doReturn(book).when(bookDao).getBook(1);
+        doReturn(viewModel).when(bookDao).addBook(book2);
+        doReturn(viewModel).when(bookDao).getBook(1);
         doReturn(bList).when(bookDao).getAllBooks();
     }
 
     @Before
     public void setUp() throws Exception {
         setUpBookDaoMock();
-        serviceLayer = new ServiceLayer(bookDao);
+        serviceLayer = new ServiceLayer(bookDao, client);
     }
 
     @Test
     public void saveBook() {
         ViewModel viewModel = new ViewModel();
-        viewModel.setB
     }
 
     @Test
