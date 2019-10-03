e72e6c763bda53e055fb1b35f930129ffb94419d
ace <allisterg@gmail.com>
2019-10-03 11:37:07 -0400

    add find, findall, and delete to service layer

--- book-service/src/main/java/com/company/bookservice/service/ServiceLayer.java
+++ book-service/src/main/java/com/company/bookservice/service/ServiceLayer.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ServiceLayer {
@@ -42,6 +43,10 @@ public class ServiceLayer {
 
     public List<ViewModel> findAllBooks(){
 
+        return dao.getAllBooks().stream()
+                .map(this::buildViewModel)
+                .collect(Collectors.toList());
+
     }
 
     @Transactional
@@ -51,6 +56,11 @@ public class ServiceLayer {
 
     @Transactional
     public void removeBook(int id){
+
+        getNotesByBook(id)
+                .stream()
+                .forEach(note -> client.deleteNoteFromDB(note.getNoteId()));
+        dao.deleteBook(id);
 
     }
 
