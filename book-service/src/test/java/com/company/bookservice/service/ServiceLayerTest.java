d87d99e1103dab6cdeb1b85f41d674baacd5656b
rfenix3 <richard.fenix@gmail.com>
2019-10-03 10:47:37 -0400

    Service Layer Testing.

--- book-service/src/test/java/com/company/bookservice/service/ServiceLayerTest.java
+++ book-service/src/test/java/com/company/bookservice/service/ServiceLayerTest.java
@@ -1,16 +1,53 @@
 package com.company.bookservice.service;
 
+import com.company.bookservice.DAO.BookDao;
+import com.company.bookservice.DAO.BookImplemention;
+import com.company.bookservice.DTO.Book;
+import com.company.bookservice.ViewModel.ViewModel;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
 public class ServiceLayerTest {
+
+    private ServiceLayer serviceLayer;
+    private BookDao bookDao;
+
+    private void setUpBookDaoMock(){
+        bookDao = mock(BookImplemention.class);
+
+        Book book = new Book();
+        book.setBookId(1);
+        book.setTitle("Sample Title");
+        book.setAuthor("Sample Author");
+
+        Book book2 = new Book();
+        book2.setTitle("Sample Title");
+        book2.setAuthor("Sample Author");
+
+        List<Book> bList = new ArrayList<>();
+        bList.add(book);
+
+        doReturn(book).when(bookDao).addBook(book2);
+        doReturn(book).when(bookDao).getBook(1);
+        doReturn(bList).when(bookDao).getAllBooks();
+    }
 
     @Before
     public void setUp() throws Exception {
+        setUpBookDaoMock();
+        serviceLayer = new ServiceLayer(bookDao);
     }
 
     @Test
     public void saveBook() {
+        ViewModel viewModel = new ViewModel();
+        viewModel.setB
     }
 
     @Test
